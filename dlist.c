23d6c37cbc563d0d7fb65f3958ee20fee2440a17
Oskar Tengberg <oste0002@gmail.com>
2017-01-17 22:46:59 +0100

    Commenting Is stable.

--- dlist.c
+++ dlist.c
@@ -16,193 +16,192 @@
 static int32_t intern_new_hash(dlist_list *list, dlist_link *link);
 static int32_t intern_del_hash(dlist_list *list, hash_id *id);
 static int32_t intern_hash_exists(const dlist_list *list,
-		const hash_id id, uint32_t *index);
+    const hash_id id, uint32_t *index);
 static int32_t intern_avail_hash(const dlist_list *list, hash_id *id,
-		uint32_t *index);
+    uint32_t *index);
 
 
 dlist_list *dlist_init(uint32_t init_links,
-		uint32_t max_links, size_t data_size) {
+    uint32_t max_links, size_t data_size) {
 
-	size_t carrier_size = sizeof(dlist_link) + data_size;
+  size_t carrier_size = sizeof(dlist_link) + data_size;
 
-	dlist_list *list = (dlist_list *)calloc(1,sizeof(dlist_list));
-	list->p_head_list = prealloc_init(init_links, max_links, carrier_size);
-	list->top = NULL;
-	list->init_links = init_links;
-	list->max_links = max_links;
-	list->hash_links = max_links*2;
+  dlist_list *list = (dlist_list *)calloc(1,sizeof(dlist_list));
+  list->p_head_list = prealloc_init(init_links, max_links, carrier_size);
+  list->top = NULL;
+  list->init_links = init_links;
+  list->max_links = max_links;
+  list->hash_links = max_links*2;
 
-	list->data_size = data_size;
-	list->hash_is_active = false;
-	return list;
+  list->data_size = data_size;
+  list->hash_is_active = false;
+  return list;
 }
 
 
-dlist_link *dlist_ins(dlist_list *list, void *data) {
-	prealloc_cell *p_cell;
-	dlist_link *link;
-	dlist_linkCarrier *carrier;
+dlist_link *dlist_ins(dlist_list *list, const void *data) {
+  prealloc_cell *p_cell;
+  dlist_link *link;
+  dlist_linkCarrier *carrier;
 
-	if ( (p_cell = prealloc_new(list->p_head_list)) == NULL )
-		return(NULL);
+  if ( (p_cell = prealloc_new(list->p_head_list)) == NULL )
+    return(NULL);
 
-	carrier = (dlist_linkCarrier *)prealloc_memget(p_cell);
-	link = &carrier->link;
-	link->data = &carrier->data;
-	link->p_cell = p_cell;
-	link->next = list->top;
-	if (link->next != NULL)
-		link->next->prev = link;
-	link->prev = NULL;
-	memcpy(link->data,data,list->data_size);
-	list->top = link;
+  carrier = (dlist_linkCarrier *)prealloc_memget(p_cell);
+  link = &carrier->link;
+  link->data = &carrier->data;
+  link->p_cell = p_cell;
+  link->next = list->top;
+  if (link->next != NULL)
+    link->next->prev = link;
+  link->prev = NULL;
+  memcpy(link->data,data,list->data_size);
+  list->top = link;
 
-	if ( list->hash_is_active == true )
-		if (intern_new_hash(list, link) == 0 )
-			fprintf(stderr,"Error: Hash table is full!\n");
+  if ( list->hash_is_active == true )
+    if (intern_new_hash(list, link) == 0 )
+      fprintf(stderr,"Error: Hash table is full!\n");
 
-	return(link);
+  return(link);
 }
 
 
 void dlist_del(dlist_list *list, dlist_link *link) {
 
-	if ( list->hash_is_active == true ) {
-		intern_del_hash(list, link->id);
-		link->id = NULL; }
+  if ( list->hash_is_active == true ) {
+    intern_del_hash(list, link->id);
+    link->id = NULL; }
 
-	if (link->prev != NULL)
-		link->prev->next = link->next;
-	if (link->next != NULL)
-		link->next->prev = link->prev;
+  if (link->prev != NULL)
+    link->prev->next = link->next;
+  if (link->next != NULL)
+    link->next->prev = link->prev;
 
-	if (link->prev == NULL)
-		list->top = link->next;
+  if (link->prev == NULL)
+    list->top = link->next;
 
-	prealloc_del(list->p_head_list, link->p_cell);
-	return;
+  prealloc_del(list->p_head_list, link->p_cell);
+  return;
 }
 
 
 dlist_link *dlist_mtf(dlist_list *list, dlist_link *link) {
 
-	if (link->prev == NULL)
-		return(link);
+  if (link->prev == NULL)
+    return(link);
 
-	if (link->next != NULL)
-		link->next->prev = link->prev;
-	if (link->prev != NULL)
-		link->prev->next = link->next;
+  if (link->next != NULL)
+    link->next->prev = link->prev;
+  if (link->prev != NULL)
+    link->prev->next = link->next;
 
-	link->next = list->top;
-	if (link->next != NULL)
-		link->next->prev = link;
+  link->next = list->top;
+  if (link->next != NULL)
+    link->next->prev = link;
 
-	link->prev = NULL;
-	list->top = link;
+  link->prev = NULL;
+  list->top = link;
 
-	return(link);
+  return(link);
 }
 
 
-void *dlist_get(dlist_link *link) {
-	return(link->data);
+void *dlist_get(const dlist_link *link) {
+  return(link->data);
 }
 
 
-uint32_t dlist_get_id(dlist_link *link) {
-	return(link->id != NULL ? *link->id : 0);
+uint32_t dlist_get_id(const dlist_link *link) {
+  return(link->id != NULL ? *link->id : 0);
 }
 
 
-dlist_link *dlist_next(dlist_link *link) {
-	return(link->next);
+dlist_link *dlist_lookup(const dlist_list *list, const uint32_t id) {
+  uint32_t index;
+
+  if (list->hash_is_active != true)
+    return(NULL);
+
+  intern_hash_exists(list, id, &index);
+  return(index != 0U ? list->hash_pool[index].ptr : NULL);
 }
 
 
-dlist_link *dlist_prev(dlist_link *link) {
-	return(link->prev);
+dlist_link *dlist_next(const dlist_link *link) {
+  return(link->next);
 }
 
 
-dlist_link *dlist_circ(dlist_list *list, dlist_link *link) {
-	return(link->next != NULL ? link->next : list->top);
+dlist_link *dlist_prev(const dlist_link *link) {
+  return(link->prev);
 }
 
 
-uint32_t dlist_links(dlist_list *list) {
-	return(list->p_head_list->num_cells);
+dlist_link *dlist_circ(const dlist_list *list, const dlist_link *link) {
+  return(link->next != NULL ? link->next : list->top);
+}
+
+
+uint32_t dlist_links(const dlist_list *list) {
+  return(list->p_head_list->num_cells);
 }
 
 
 void dlist_destroy(dlist_list *list) {
-	prealloc_destroy(list->p_head_list);
-	if (list->hash_is_active == true)
-		free(list->hash_pool);
-	free(list);
-	return;
+  prealloc_destroy(list->p_head_list);
+  if (list->hash_is_active == true)
+    free(list->hash_pool);
+  free(list);
+  return;
 }
 
 
-dlist_link *dlist_top(dlist_list *list) {
-	return(list->top);
+dlist_link *dlist_top(const dlist_list *list) {
+  return(list->top);
 }
 
 
-int dlist_exist(dlist_link *link) {
-	return(link != NULL);
+int dlist_exist(const dlist_link *link) {
+  return(link != NULL);
 }
 
 
-int dlist_is_indexed(dlist_list *list) {
-	return(list->hash_is_active == true ? 1 : 0);
+int dlist_is_indexed(const dlist_list *list) {
+  return(list->hash_is_active == true ? 1 : 0);
 }
 
 
 void dlist_index(dlist_list *list) {
-	dlist_link *link;
+  dlist_link *link;
 
-	if (list->hash_is_active == false)
-		list->hash_pool = (dlist_hash *)calloc(list->hash_links,
-				sizeof(dlist_hash));
-	else
-		memset(list->hash_pool, 0, list->hash_links * sizeof(dlist_hash));
+  if (list->hash_is_active == false)
+    list->hash_pool = (dlist_hash *)calloc(list->hash_links,
+        sizeof(dlist_hash));
+  else
+    memset(list->hash_pool, 0, list->hash_links * sizeof(dlist_hash));
 
-	for (link=dlist_top(list);dlist_exist(link);link=dlist_next(link))
-		if ( intern_new_hash(list, link) == 0 )
-			fprintf(stderr,"Error: Hash table is full!\n");
+  for (link=dlist_top(list);dlist_exist(link);link=dlist_next(link))
+    if ( intern_new_hash(list, link) == 0 )
+      fprintf(stderr,"Error: Hash table is full!\n");
 
-	list->hash_is_active = true;
-	return;
+  list->hash_is_active = true;
+  return;
 }
 
 
 void dlist_drop_index(dlist_list *list) {
-	dlist_link *link;
+  dlist_link *link;
 
-	if (list->hash_is_active == true) {
-		free(list->hash_pool);
-		list->hash_is_active = false; }
+  if (list->hash_is_active == true) {
+    free(list->hash_pool);
+    list->hash_is_active = false; }
 
-	for (link=dlist_top(list);dlist_exist(link);link=dlist_next(link))
-		link->id = NULL;
+  for (link=dlist_top(list);dlist_exist(link);link=dlist_next(link))
+    link->id = NULL;
 
-	list->hash_is_active = false;
-	return;
+  list->hash_is_active = false;
+  return;
 }
-
-
-dlist_link *dlist_lookup(dlist_list *list, uint32_t id) {
-	uint32_t index;
-
-	if (list->hash_is_active != true)
-		return(NULL);
-
-	intern_hash_exists(list, id, &index);
-	return(index != 0U ? list->hash_pool[index].ptr : NULL);
-}
-
 
 
 /* INTERNAL FUNCTIONS */
@@ -212,55 +211,55 @@ dlist_link *dlist_lookup(dlist_list *list, uint32_t id) {
  * Initializes hash for the dlist_link that 'link' points at.
  */
 static int32_t intern_new_hash(dlist_list *list, dlist_link *link) {
-	dlist_hash *hash;
-	hash_id id = 0;
-	struct timeval tim;
-	uint32_t index;
-	int i=0;
+  dlist_hash *hash;
+  hash_id id = 0;
+  struct timeval tim;
+  uint32_t index;
+  int i=0;
 
-	gettimeofday(&tim, NULL);
-	srand(tim.tv_usec | tim.tv_sec);
-	id = rand();
-	if ( (i = intern_avail_hash(list, &id, &index)) == -1 )
-		return(-1);
+  gettimeofday(&tim, NULL);
+  srand(tim.tv_usec | tim.tv_sec);
+  id = rand();
+  if ( (i = intern_avail_hash(list, &id, &index)) == -1 )
+    return(-1);
 
-	hash = &list->hash_pool[index];
-	hash->id = id;
-	hash->ptr = link;
-	link->id = &hash->id;
+  hash = &list->hash_pool[index];
+  hash->id = id;
+  hash->ptr = link;
+  link->id = &hash->id;
 #ifdef DLIST_DEBUG
-	printf("Hash iterations: %u\n", i);
-	printf("id: %u\tindex: %u\n", *link->id, index);
+  printf("Hash iterations: %u\n", i);
+  printf("id: %u\tindex: %u\n", *link->id, index);
 #endif
-	return(i);
+  return(i);
 }
 
 
 static int32_t intern_del_hash(dlist_list *list, hash_id *id) {
-	uint32_t index;
+  uint32_t index;
 
-	if ( intern_hash_exists(list, *id, &index) == 0 )
-		return(1);
-	list->hash_pool[index].ptr = NULL;
-	list->hash_pool[index].id = 0;
-	return(0);
+  if ( intern_hash_exists(list, *id, &index) == 0 )
+    return(1);
+  list->hash_pool[index].ptr = NULL;
+  list->hash_pool[index].id = 0;
+  return(0);
 }
 
 
 /* intern_hash_exists
  *
- * Return:	0		- Hash does not exist
- *					1		- Hash exists
+ * Return:  0   - Hash does not exist
+ *          1   - Hash exists
  */
 static int32_t intern_hash_exists(const dlist_list *list, const hash_id id,
-		uint32_t *index) {
+    uint32_t *index) {
 
-	*index = id % list->hash_links;
-	if ( list->hash_pool[*index].id != id ) {
-		*index = 0;
-		return(1); }
-	else
-		return(0);
+  *index = id % list->hash_links;
+  if ( list->hash_pool[*index].id != id ) {
+    *index = 0;
+    return(1); }
+  else
+    return(0);
 }
 
 
@@ -268,25 +267,25 @@ static int32_t intern_hash_exists(const dlist_list *list, const hash_id id,
  * Finds a id and corresponding hash index, runs at most
  * NEW_HASH_MAX_ITERATIONS times.
  *
- * Return:	0		- Hash does not exist
- *					i		- Number of iteratins performed in order to find available hash
+ * Return:  0   - Hash does not exist
+ *          i   - Number of iteratins performed in order to find available hash
  */
 static int32_t intern_avail_hash(const dlist_list *list, hash_id *id,
-		uint32_t *index) {
-	uint32_t i;
+    uint32_t *index) {
+  uint32_t i;
 
-	for (*index = *id % list->hash_links, i=1;
-			(list->hash_pool[*index].id != 0U || *index == 0) &&
-			i<NEW_HASH_MAX_ITERATIONS;
-			*index = *id % list->hash_links, i++ )
-		*id = rand_r(id);
+  for (*index = *id % list->hash_links, i=1;
+      (list->hash_pool[*index].id != 0U || *index == 0) &&
+      i<NEW_HASH_MAX_ITERATIONS;
+      *index = *id % list->hash_links, i++ )
+    *id = rand_r(id);
 
-	if (i >= NEW_HASH_MAX_ITERATIONS) {
-		id = NULL;
-		index = NULL;
-		return(-1); }
-	else
-		return(i);
+  if (i >= NEW_HASH_MAX_ITERATIONS) {
+    id = NULL;
+    index = NULL;
+    return(-1); }
+  else
+    return(i);
 }
 
 
--- dlist.h
+++ dlist.h
@@ -16,219 +16,219 @@ typedef struct Dlist_Hash dlist_hash;
 
 
 struct Dlist_Link {
-	struct Dlist_Link *next;
-	struct Dlist_Link *prev;
-	prealloc_cell *p_cell;
-	hash_id *id;
-	void *data;
+  struct Dlist_Link *next;
+  struct Dlist_Link *prev;
+  prealloc_cell *p_cell;
+  hash_id *id;
+  void *data;
 };
 
 struct Dlist_LinkCarrier {
-	dlist_link link;
-	void *data;
+  dlist_link link;
+  void *data;
 };
 
 struct Dlist_Hash {
-	struct Dlist_Link *ptr;
-	hash_id id;
+  struct Dlist_Link *ptr;
+  hash_id id;
 };
 
 struct Dlist_List{
-	dlist_link *top;
-	prealloc_head *p_head_list;
-	uint32_t init_links;
-	uint32_t max_links;
-	uint32_t hash_links;
-	size_t data_size;
-	bool hash_is_active;
-	dlist_hash *hash_pool;
+  dlist_link *top;
+  prealloc_head *p_head_list;
+  uint32_t init_links;
+  uint32_t max_links;
+  uint32_t hash_links;
+  size_t data_size;
+  bool hash_is_active;
+  dlist_hash *hash_pool;
 };
 
 /* DLIST_INIT - Initializes a linked list of preallocated items. The list will
  * automatically extend if needed.
  *
- *	uint32_t init_links		-	Number of links that is allocated on list
- *													initialization and list extension.
- *	uint32_t max_link			-	Number of links that the list is guranteed to	be able
- *													to extend into.
- *	ssize_t data_size			-	Size of the largest amount of data that could be
- *													inserted into	the list.
+ *  uint32_t init_links   - Number of links that is allocated on list
+ *                          initialization and list extension.
+ *  uint32_t max_link     - Number of links that the list is guranteed to be able
+ *                          to extend into.
+ *  ssize_t data_size     - Size of the largest amount of data that could be
+ *                          inserted into the list.
  *
- * Return:	A pointer to the list.
+ * Return:  A pointer to the list.
  */
-dlist_list *dlist_init(uint32_t init_links,	uint32_t max_links,
-		size_t data_size);
+dlist_list *dlist_init(uint32_t init_links, uint32_t max_links,
+    size_t data_size);
 
 
 /* DLIST_INS - Inserts data into the list the. The size of the data to be
  * inserted into the list cannot exceed 'data_size' bytes, which is specified on
  * list initialization.
  *
- *	dlist_list *list			- A pointer to the list which the data will be
- *													inserted into.
- *	void *data						-	A pointer to the data which will be inserted.
+ *  dlist_list *list  - A pointer to the list which the data will be
+ *                      inserted into.
+ *  const void *data  - A pointer to the data which will be inserted.
  *
- * Return:	A pointer to the link that holds the inserted data.
- * Note:		The memory is copied from the provided data pointer into internal
- *					memory. Therefore, the dlist does not keep track of the original
- *					memory address.
+ * Return:  A pointer to the link that holds the inserted data.
+ * Note:    The memory is copied from the provided data pointer into internal
+ *          memory. Therefore, the dlist does not keep track of the original
+ *          memory address.
  */
-dlist_link *dlist_ins(dlist_list *list, void *data);
+dlist_link *dlist_ins(dlist_list *list, const void *data);
 
 
 /* DLIST_DEL - Deletes a link from the list.
  *
- *	dlist_list *list				-	A pointer to the list which hold the link that will
- *													be deleted.
- *	dlist_link *link				- A pointer to the link that will be deleted. This
- *													pointer may be collected from 'dlist_ins()',
- *													'dlist_lookup()', 'dlist_next()', 'dlist_prev()' or
- *													'dlist_circ()'.
+ *  dlist_list *list        - A pointer to the list which hold the link that will
+ *                          be deleted.
+ *  dlist_link *link        - A pointer to the link that will be deleted. This
+ *                          pointer may be collected from 'dlist_ins()',
+ *                          'dlist_lookup()', 'dlist_next()', 'dlist_prev()' or
+ *                          'dlist_circ()'.
  */
 void dlist_del(dlist_list *list, dlist_link *link);
 
 
 /* DLIST_MTF - Move To Front of list.
  *
- *	dlist_list *list		- A pointer to the list.
- *	dlist_list *link		- A pointer to the link to be moved to front.
+ *  dlist_list *list    - A pointer to the list.
+ *  dlist_list *link    - A pointer to the link to be moved to front.
  *
- * Return:	A pointer to the link that is moved to front.
+ * Return:  A pointer to the link that is moved to front.
  */
 dlist_link *dlist_mtf(dlist_list *list, dlist_link *link);
 
 
 /* DLIST_GET - Get the value of a specific link
  *
- *	dlist_link *link	-	The link that contains data.
+ *  dlist_link *link  - The link that contains data.
  *
- * Return:	A pointer to a memory segment that holds the inserted data.
- * Note:		The returned pointer is not the same pointer as was inserted.
+ * Return:  A pointer to a memory segment that holds the inserted data.
+ * Note:    The returned pointer is not the same pointer as was inserted.
  */
-void *dlist_get(dlist_link *link);
+void *dlist_get(const dlist_link *link);
 
 
 /* DLIST_GET_ID - Get the id of a link if the list is indexed.
  *
- *	dlist_link *link	- A pointer to a link of an indexed list.
+ *  dlist_link *link  - A pointer to a link of an indexed list.
  *
  * Return:
- *	uint32_t		SUCCESS:	ID >= 1, corresponding to the inserted link pointer.
- *							FAILURE:	0, This may occur if the link is not indexed.
+ *  uint32_t    SUCCESS:  ID >= 1, corresponding to the inserted link pointer.
+ *              FAILURE:  0, This may occur if the link is not indexed.
  */
-uint32_t dlist_get_id(dlist_link *link);
+uint32_t dlist_get_id(const dlist_link *link);
 
 
 /* DLIST_LOOKUP - Searches for a link pointer by its unique ID. The 
  *
- *	dlist_list *list	-	A list pointer.
- *	uint32_t id				-	An ID of a link that is present in the list.
+ *  dlist_list *list  - A list pointer.
+ *  uint32_t id       - An ID of a link that is present in the list.
  *
  * Return:
- *		NULL					-	There is no pointer int the list that	owns the id, or the
- *										list is not indexed.
- *	  dlist_list *	-	The pointer to the link which owns the provided id.
+ *    NULL          - There is no pointer int the list that owns the id, or the
+ *                    list is not indexed.
+ *    dlist_list *  - The pointer to the link which owns the provided id.
  */
-dlist_link *dlist_lookup(dlist_list *list, uint32_t id);
+dlist_link *dlist_lookup(const dlist_list *list, const uint32_t id);
 
 
 /* DLIST_NEXT - Next link in list.
  *
- *	dlist_link *link	-	A pointer to a link.
+ *  dlist_link *link  - A pointer to a link.
  *
- * Return:	The link next of the input link. Or NULL if the link is last in list.
+ * Return:  The link next of the input link. Or NULL if the link is last in list.
  */
-dlist_link *dlist_next(dlist_link *link);
+dlist_link *dlist_next(const dlist_link *link);
 
 
 /* DLIST_PREV - Previous link in list.
  *
- *	dlist_link *link	-	A pointer to a link.
+ *  dlist_link *link  - A pointer to a link.
  *
- * Return:	The link previous to the input link. Or NULL if the link is first
- *					in list.
+ * Return:  The link previous to the input link. Or NULL if the link is first
+ *          in list.
  */
-dlist_link *dlist_prev(dlist_link *link);
+dlist_link *dlist_prev(const dlist_link *link);
 
 
 /* DLIST_CIRC - Same as 'DLIST_NEXT()' except that the first link is returned
  * if the input link is the last link of the list.
  *
- *	dlist_list *list	-	A pointer to the list which holds the link.
- *	dlist_link *link	-	A pointer to a link.
+ *  dlist_list *list  - A pointer to the list which holds the link.
+ *  dlist_link *link  - A pointer to a link.
  *
- * Return:	The link pointer next of the input link. Or the first link pointer
- *					of 'list' if the input link is last in list.
+ * Return:  The link pointer next of the input link. Or the first link pointer
+ *          of 'list' if the input link is last in list.
  */
-dlist_link *dlist_circ(dlist_list *list, dlist_link *link);
+dlist_link *dlist_circ(const dlist_list *list, const dlist_link *link);
 
 
 /* DLIST_LINKS - The number of inserted links in a list.
  *
- *	dlist_list *list	-	A list pointer.
+ *  dlist_list *list  - A list pointer.
  *
  * Return: The number of inserted links.
  */
-uint32_t dlist_links(dlist_list *list);
+uint32_t dlist_links(const dlist_list *list);
 
 
 /* DLIST_DESTROY - Destroy the list. Should be called when the list is not used
  * any longer.
  *
- *	dlist_list *list	-	A list pointer.
+ *  dlist_list *list  - A list pointer.
  */
 void dlist_destroy(dlist_list *list);
 
 
 /* DLIST_TOP - The address of the first pointer of the list.
  *
- *	dlist_list *list	-	A list pointer
+ *  dlist_list *list  - A list pointer
  *
  * Return: A pointer of the first link in the list. May be NULL if the list
  * is empty.
  */
-dlist_link *dlist_top(dlist_list *list);
+dlist_link *dlist_top(const dlist_list *list);
 
 
 /* DLIST_EXIST - Tests if a link exists or not. This function should only be
  * used directly on the return of 'DLIST_NEXT()' and 'DLIST_PREV()' functions.
  *
- *	dlist_link *link	-	A pointer to the link which will be tested.
+ *  dlist_link *link  - A pointer to the link which will be tested.
  *
- * Return:	0	-	The link does not exist.
- *					1	-	The link exists.
+ * Return:  0 - The link does not exist.
+ *          1 - The link exists.
  *
- * Note:		This function basically tests if the link pointer is NULL or not.
- *					A pointer to something other than NULL will be returned as 1 even
- *					though it is not a valid link.
+ * Note:    This function basically tests if the link pointer is NULL or not.
+ *          A pointer to something other than NULL will be returned as 1 even
+ *          though it is not a valid link.
  */
-int dlist_exist(dlist_link *link);
+int dlist_exist(const dlist_link *link);
 
 
 /* DLIST_IS_INDEXED - Tests if the list is indexed
  *
- *	dlist_link *link	-	A pointer to the link which will be tested.
+ *  dlist_link *link  - A pointer to the link which will be tested.
  *
- * Return:	0	-	The list is not indexed.
- *					1	-	The list is indexed.
+ * Return:  0 - The list is not indexed.
+ *          1 - The list is indexed.
  */
-int dlist_is_indexed(dlist_list *list);
+int dlist_is_indexed(const dlist_list *list);
 
 
 /* DLIST_INDEX - Indexes the list
  *
- *	dlist_link *link	-	A pointer to the list which will be indexed.
+ *  dlist_link *link  - A pointer to the list which will be indexed.
  *
  * Note: Indexation of a list takes up some space, proportional to the maximum
- *			 number of links allowed in the list. This is more than, or equal to,
- *			 8 bytes per link allowed.
+ *       number of links allowed in the list. This is more than, or equal to,
+ *       8 bytes per link allowed.
  */
 void dlist_index(dlist_list *list);
 
 
 /* DLIST_DROP_INDEX - Drops the indexing of the list in order
  *
- *	dlist_link *link	-	A pointer to the list which will be unindexed.
+ *  dlist_link *link  - A pointer to the list which will be unindexed.
  */
 void dlist_drop_index(dlist_list *list);
 
